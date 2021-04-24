836a3a54bb1e5636a4af192508941ca130b3cfa4
rong fengliang <1141591465@qq.com>
2021-04-24 19:39:36 +0800

    init project

---
+++ README.md
@@ -0,0 +1,30 @@
+# cubejs-schemaversion-cache
+
+using cache-manager for cache multi tennant schemaversion
+
+
+## Usage
+
+```code
+yarn add @dalongrong/cubejs-schemaversion-cache
+
+cube.js
+
+// Cube.js configuration options: https://cube.dev/docs/config
+const {pause,schemaVersion} = require("@dalongrong/cubejs-schemaversion-cache")
+
+// one demo func 
+async function fetchVersion(securityContext){
+  pause(3000)
+  console.log("call version")
+  return `"dalong"---${parseInt(Math.random()*100)}`
+}
+module.exports = {
+  schemaVersion: async ({ securityContext }) =>{
+    return schemaVersion(securityContext,fetchVersion)
+  },
+  contextToAppId: ({ securityContext }) => {
+   return  `CUBEJS_APP_${securityContext.user_id}`
+  }
+};
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+const cacheManager = require('cache-manager');
+const crypto = require('crypto');
+const redisStore = require('cache-manager-ioredis');
+const redisCache = cacheManager.caching({
+  store: redisStore,
+  host: process.env.REDIS_HOST || "localhost", // default value
+  port: process.env.REDIS_PORT? parseInt(process.env.REDIS_PORT):6379, // default value
+  db: process.env.REDIS_DB? parseInt(process.env.REDIS_DB):0,
+  password: process.env.REDIS_PASS || null,
+  ttl: process.env.REDIS_TTL? parseInt(process.env.REDIS_TTL):30
+});
+/**
+ * pasuse helper func
+ * @param {pause ms} ms 
+ * @returns promise
+ */
+function pause(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+module.exports = {
+    schemaVersion: async (securityContext,func)=> {
+         let cachekey = `SCHEMAVERSION_${crypto.createHash('md5').update(JSON.stringify(securityContext)).digest('hex')}`
+         return redisCache.wrap(cachekey, function() {
+          return func(securityContext);
+        });
+    },
+    pause:pause
+}
