89fc1dad790dda8df0a689f10f3e6502acfe68c8
rong fengliang <1141591465@qq.com>
2021-04-29 19:58:23 +0800

    add dotenv

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+require('dotenv').config()
 const cacheManager = require('cache-manager');
 const crypto = require('crypto');
 const redisStore = require('cache-manager-ioredis');
-const env = require('env-var');
-
 const redisCache = cacheManager.caching({
   store: redisStore,
-  host: env.get('REDIS_HOST') || 'localhost', // default value
-  port: env.get('REDIS_PORT') ? parseInt(env.get('REDIS_PORT')) : 6379, // default value
-  db: env.get('REDIS_DB') ? parseInt(env.get('REDIS_DB')) : 0,
-  password: env.get('REDIS_PASS') || null,
-  ttl: env.get('REDIS_TTL') ? parseInt(env.get('REDIS_TTL') ) : 300
+  host: process.env.REDIS_HOST ||  'localhost', // default value
+  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379, // default value
+  db:  process.env.REDIS_DB ? parseInt(process.env.REDIS_DB) : 0,
+  password: process.env.REDIS_DB.REDIS_PASS || null,
+  ttl: process.env.REDIS_TTL ? parseInt(process.env.REDIS_TTL) : 300
 });
 /**
  * pasuse helper func
@@ -23,20 +22,20 @@ function pause(ms) {
 module.exports = {
   schemaVersion: async (securityContext, func) => {
     let cachekey = `SCHEMAVERSION_${crypto.createHash('md5').update(JSON.stringify(securityContext)).digest('hex')}`
-    return  redisCache.wrap(cachekey, function () {
+    return redisCache.wrap(cachekey, function () {
       return func(securityContext);
     });
   },
-  validateKey: async (jwt,func) => {
+  validateKey: async (jwt, func) => {
     // cache key with jwt md5 
     let cachekey = `JWTKEY_${crypto.createHash('md5').update(JSON.stringify(jwt)).digest('hex')}`
-    return  redisCache.wrap(cachekey, function () {
+    return redisCache.wrap(cachekey, function () {
       return func(jwt);
     });
-  }, 
+  },
   scheduledRefreshContexts: async (func) => {
     let cachekey = `SCHEDULEDREFRESHCONTEXTS_ITEMS`
-    return  redisCache.wrap(cachekey, function () {
+    return redisCache.wrap(cachekey, function () {
       return func();
     });
   },
